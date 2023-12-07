ee148b3e6c0742fd7f03a5e6cc8fa055ff1bb84c
Alfu <Alfulinku@gmail.com>
2023-12-07 10:56:49 +0100

    adapt contracts to new directory structure

--- src/morpho/BProtocolMorphoAllocator.sol
+++ src/morpho/BProtocolMorphoAllocator.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.2 <0.9.0;
 
-import {ISPythia, Signature, RiskData} from "./interfaces/ISPythia.sol";
-import {IMetaMorpho, MarketAllocation, Id, MarketParams} from "./interfaces/IMetaMorpho.sol";
-import {RiskyMath} from "./lib/RiskyMath.sol";
-import {MorphoLib} from "./lib/MorphoLib.sol";
+import {Pythia} from "../core/Pythia.sol";
+import {SmartLTV} from "../core/SmartLTV.sol";
+import {IMetaMorpho, MarketAllocation, Id, MarketParams} from "../external/IMetaMorpho.sol";
+import {RiskyMath} from "../lib/RiskyMath.sol";
+import {MorphoLib} from "../lib/MorphoLib.sol";
 
 /*  
 USDC/sDAI
@@ -28,26 +29,26 @@ marketid: 0xbc6d1789e6ba66e5cd277af475c5ed77fcf8b084347809d9d92e400ebacbdd10
 contract BProtocolMorphoAllocator {
   using MorphoLib for MarketParams;
 
-  ISPythia immutable SPYTHIA;
+  SmartLTV immutable SMART_LTV;
   address immutable TRUSTED_RELAYER;
   address immutable METAMORPHO_VAULT;
   uint256 immutable MIN_CLF = 3;
 
   error INVALID_NUMBER_OF_RISK_DATA(uint256 a);
 
-  constructor(ISPythia spythia, address relayer, address morphoVault) {
-    SPYTHIA = spythia;
+  constructor(SmartLTV smartLTV, address relayer, address morphoVault) {
+    SMART_LTV = smartLTV;
     TRUSTED_RELAYER = relayer;
     METAMORPHO_VAULT = morphoVault;
   }
 
   function checkAndReallocate(
     MarketAllocation[] calldata allocations,
-    RiskData[] calldata riskDatas,
-    Signature[] calldata signatures
+    Pythia.RiskData[] calldata riskDatas,
+    Pythia.Signature[] calldata signatures
   ) external {
-    if(allocations.length != riskDatas.length) {
-        revert INVALID_NUMBER_OF_RISK_DATA(250);
+    if (allocations.length != riskDatas.length) {
+      revert INVALID_NUMBER_OF_RISK_DATA(250);
     }
 
     require(
@@ -65,9 +66,9 @@ contract BProtocolMorphoAllocator {
 
   function _checkAllocationRisk(
     MarketAllocation memory allocation,
-    RiskData memory riskData,
-    Signature memory signature
-  ) private {
+    Pythia.RiskData memory riskData,
+    Pythia.Signature memory signature
+  ) private view {
     require(
       allocation.marketParams.collateralToken == riskData.collateralAsset,
       "Allocation collateral token != riskData.collateralAsset"
@@ -77,38 +78,24 @@ contract BProtocolMorphoAllocator {
       "allocation.loanToken != riskData.debtAsset"
     );
 
-    // Verify if the signature comes from the trusted relayer
-    address signer = SPYTHIA.getSigner(
+    // get market config from the vault
+    Id marketId = allocation.marketParams.id();
+    (uint184 cap, , ) = IMetaMorpho(METAMORPHO_VAULT).config(marketId);
+
+    uint256 d = cap; // supplyCap
+    uint256 beta = 15487; // TODO REAL liquidation bonus
+
+    uint recommendedLtv = SMART_LTV.ltv(
+      riskData.collateralAsset,
+      riskData.debtAsset,
+      d,
+      beta,
+      MIN_CLF,
       riskData,
       signature.v,
       signature.r,
       signature.s
     );
-    require(signer == TRUSTED_RELAYER, "invalid signer");
-
-    // timeout
-    require(riskData.lastUpdate + 1 days >= block.timestamp, "timeout");
-
-    // chain id
-    require(riskData.chainId == block.chainid, "invalid chainId");
-
-    // get market config from the vault
-    Id marketId = allocation.marketParams.id();
-    (uint184 cap, , ) = IMetaMorpho(METAMORPHO_VAULT).config(marketId);
-
-    uint256 sigma = riskData.volatility;
-    uint256 l = riskData.liquidity;
-    uint256 d = cap; // supplyCap
-    uint256 beta = 15487; // liquidation bonus
-
-    // LTV  = e ^ (-c * sigma / sqrt(l/d)) - beta
-    uint256 cTimesSigma = (MIN_CLF * sigma) / 1e18;
-    uint256 sqrtValue = RiskyMath.sqrt((1e18 * l) / d) * 1e9;
-    uint256 mantissa = ((1 << 59) * cTimesSigma) / sqrtValue;
-
-    uint256 expResult = RiskyMath.generalExp(mantissa, 59);
-
-    uint256 recommendedLtv = (1e18 * (1 << 59)) / expResult - beta;
 
     // check if the current ltv is lower or equal to the recommended ltv
     require(
