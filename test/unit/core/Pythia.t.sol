d6c1975b42dd5769278a1fde9afff61386b3b981
Alfu <Alfulinku@gmail.com>
2023-12-11 10:56:46 +0100

    add full coverage test for Pythia.sol

--- test/unit/core/Pythia.t.sol
+++ test/unit/core/Pythia.t.sol
@@ -1,11 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
-import "ds-test/test.sol";
-import {Pythia} from "src/core/Pythia.sol";
-import {RiskData} from "src/interfaces/RiskData.sol";
+import {Test} from "../../../lib/forge-std/src/Test.sol";
+import {Pythia} from "../../../src/core/Pythia.sol";
+import {RiskData} from "../../../src/interfaces/RiskData.sol";
+import {ECDSA} from "../../../lib/openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
+import {MessageHashUtils} from "../../../lib/openzeppelin-contracts/contracts/utils/cryptography/MessageHashUtils.sol";
 
-contract PythiaTest is DSTest {
+contract PythiaTest is Test {
   Pythia pythia;
 
   function setUp() public {
@@ -47,5 +49,47 @@ contract PythiaTest is DSTest {
     );
 
     assertEq(pythia.hashStruct(data), hashedStruct);
+  }
+
+  function testGetSigner(
+    uint256 signerPrivateKeySeed,
+    address collateralAsset,
+    address debtAsset,
+    uint256 liquidity,
+    uint256 volatility,
+    uint256 lastUpdate,
+    uint256 chainId
+  ) public {
+    uint256 signerPrivateKey = bound(signerPrivateKeySeed, 1, 1e20);
+    address signerAddress = vm.addr(signerPrivateKey);
+
+    RiskData memory data = RiskData({
+      collateralAsset: collateralAsset,
+      debtAsset: debtAsset,
+      liquidity: liquidity,
+      volatility: volatility,
+      lastUpdate: lastUpdate,
+      chainId: chainId
+    });
+
+    // sign risk data
+    bytes32 structHash = keccak256(
+      abi.encode(
+        pythia.RISKDATA_TYPEHASH(),
+        data.collateralAsset,
+        data.debtAsset,
+        data.liquidity,
+        data.volatility,
+        data.lastUpdate,
+        data.chainId
+      )
+    );
+
+    bytes32 digest = MessageHashUtils.toTypedDataHash(pythia.DOMAIN_SEPARATOR(), structHash);
+    (uint8 v, bytes32 r, bytes32 s) = vm.sign(signerPrivateKey, digest);
+
+    address signerAccordingToPythia = pythia.getSigner(data, v, r, s);
+
+    assertEq(signerAccordingToPythia, signerAddress);
   }
 }
